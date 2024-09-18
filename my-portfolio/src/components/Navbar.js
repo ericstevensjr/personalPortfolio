1ff7989eb39bf67e16684db353d2df9a6748886e
Eric Stevens Jr. <ericstevensjrprofessional@outlook.com>
2024-09-18 15:39:20 -0400

    Added social media icons to footer.

--- my-portfolio/src/components/Navbar.js
+++ my-portfolio/src/components/Navbar.js
@@ -5,16 +5,16 @@ import cib from '../assets/cib.png';
 function Navbar() {
   return (
     <nav className="bg-gray-800 py-2 fixed w-full z-10 dark:bg-gray-900 transition-all duration-300">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4 md:px-8">
-        {/* Name and Badge for Mobile and Larger Screens */}
-        <div className="flex items-center flex-col md:flex-row space-y-2 md:space-y-0">
+      <div className="container mx-auto px-4 md:px-8 flex flex-col md:flex-row justify-between items-center">
+        {/* Name and Badge always stacked vertically */}
+        <div className="flex flex-col items-center">
           <h1 className="text-white text-3xl font-bold md:text-4xl">
             Eric Stevens Jr.
           </h1>
           <img
             src={cib}
             alt="Combat Infantry Badge"
-            className="w-16 h-auto ml-2 md:w-32 mt-1 md:mt-0"
+            className="w-40 md:w-40 h-auto mt-1"
           />
         </div>
 
