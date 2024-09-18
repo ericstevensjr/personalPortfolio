a725e7a3cdaa8aab04bddcd27ecc28f28a271b93
Eric Stevens Jr. <ericstevensjrprofessional@outlook.com>
2024-09-18 16:38:13 -0400

    Shorter about me

--- my-portfolio/src/components/AboutMe.js
+++ my-portfolio/src/components/AboutMe.js
@@ -9,17 +9,6 @@ function AboutMe() {
         <div className="flex flex-col items-center">
           <p className="text-lg leading-relaxed text-gray-700 dark:text-gray-300">
             I am a dedicated software engineer with experience in building scalable web applications...
-
-Lorem ipsum odor amet, consectetuer adipiscing elit. Faucibus pretium sagittis etiam, ut vestibulum adipiscing aptent. Taciti semper proin rhoncus ornare curabitur ullamcorper auctor natoque. Amet lacinia dui; eleifend efficitur sem commodo. Sed suscipit sit vulputate elementum commodo ligula non. Imperdiet potenti vitae finibus aenean class montes. Porta sed potenti duis dui ut aenean feugiat. Tincidunt ligula ornare rhoncus vehicula mattis rutrum fringilla tempus. Magna cras rutrum morbi tortor feugiat.
-
-Suspendisse nascetur volutpat; hendrerit at lorem faucibus. Dignissim suspendisse leo magna facilisis at rhoncus sem facilisis. Amet auctor hendrerit primis enim gravida hac. Ac vel nascetur dolor dolor dictum proin congue. Suscipit magna auctor volutpat ante mollis. Imperdiet purus vitae elementum primis, leo odio aenean dolor! Senectus tellus consectetur lectus mus ullamcorper ex.
-
-Molestie elementum vivamus condimentum id lacinia potenti varius egestas. Potenti bibendum curabitur consectetur netus consectetur elementum. Blandit volutpat cras vel commodo himenaeos nibh turpis faucibus. Ligula aptent ultrices montes proin aenean fermentum purus. Ante elementum consequat platea cras litora curabitur auctor curabitur. Sodales imperdiet nibh nascetur dictum facilisi turpis ac inceptos. Augue aptent dolor platea vitae; neque netus elit. Tellus tellus diam eleifend lobortis libero nisl facilisi? Nullam gravida tincidunt facilisis tempor, turpis eleifend risus.
-
-Blandit fames sagittis elementum pretium iaculis ante in libero. Netus dictum consequat efficitur dignissim aenean viverra sapien egestas. Dapibus neque lacus, nec primis finibus bibendum. Suspendisse taciti purus nostra aenean primis. Lacus duis nascetur torquent scelerisque accumsan tortor. Faucibus commodo odio eu vestibulum molestie eleifend. Nec laoreet inceptos laoreet cubilia pulvinar suspendisse turpis. Elit mi pulvinar mi magnis curae tempor euismod consectetur. Sodales et congue donec nisl feugiat purus est.
-
-Ex adipiscing metus consectetur per lobortis mattis. Molestie consequat natoque nisl eleifend montes. Purus dolor bibendum odio sollicitudin elementum suspendisse sapien. Maecenas ex arcu penatibus, eget libero magna. Dapibus et ullamcorper molestie ultrices tristique convallis eros mi. Tempus justo condimentum neque senectus donec odio integer quis. Lorem a scelerisque sollicitudin phasellus ornare et habitasse convallis.
-
         </p>
         </div>
       </section>
