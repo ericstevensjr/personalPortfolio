e345daccf29b5c3bc9f54dc8fd160ee31bbbe85e
Eric Stevens Jr. <ericstevensjrprofessional@outlook.com>
2024-09-18 11:11:54 -0400

    Integrated Tailwind CSS for styling

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center text-blue-500">
+      <h1 className="text-3xl font-bold">Welcome to Eric Stevens' Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
