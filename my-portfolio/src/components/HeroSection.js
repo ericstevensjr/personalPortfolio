94a44fc0f58c1b91c3adaaf0b806420068ad40e9
Eric Stevens Jr. <ericstevensjrprofessional@outlook.com>
2024-09-18 19:07:37 -0400

    Added flags to page

--- my-portfolio/src/components/HeroSection.js
+++ my-portfolio/src/components/HeroSection.js
@@ -1,23 +1,38 @@
-// src/components/HeroSection.js
+import usaFlag from "../assets/usaFlag.png"
+import flFLag from "../assets/flFlag.png"
+
 function HeroSection() {
-    return (
-      <section className="flex flex-col items-center justify-center text-center my-16">
-        <h1 className="text-5xl font-bold mb-4 text-gray-900 dark:text-gray-100 transition-colors duration-300">
-          Hi, I'm Eric Stevens
-        </h1>
-        <p className="text-xl mb-6 text-gray-700 dark:text-gray-300 transition-colors duration-300">
-          A United States Army Combat Infantryman turned Software Engineer and Businessman.
-        </p>
-        <a
-          href="#contact"
-          className="px-6 py-3 bg-blue-600 text-white rounded-full hover:bg-blue-700
-                     dark:text-gray-300 dark:hover:bg-blue-600 transition-colors duration-300"
-        >
-          Get in Touch
-        </a>
-      </section>
-    );
-  }
-  
-  export default HeroSection;
-  
+  return (
+    <section className="flex flex-col items-center justify-center text-center my-16">
+      <h1 className="text-5xl font-bold mb-4 text-gray-900 dark:text-gray-100 transition-colors duration-300">
+        Hi, I'm Eric Stevens
+      </h1>
+      <p className="text-xl mb-6 text-gray-700 dark:text-gray-300 transition-colors duration-300">
+        A United States Army Combat Infantryman turned Software Engineer and Businessman.
+      </p>
+      <a
+        href="#contact"
+        className="px-6 py-3 bg-blue-600 text-white rounded-full hover:bg-blue-700
+                   dark:text-gray-300 dark:hover:bg-blue-600 transition-colors duration-300"
+      >
+        Get in Touch
+      </a>
+
+      {/* Flags Section */}
+      <div className="flex items-center mt-6">
+        <img
+          src={usaFlag}
+          alt="United States Flag"
+          className="w-48 h-28 mr-4"
+        />
+        <img
+          src={flFLag}
+          alt="Florida State Flag"
+          className="w-46 h-28"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default HeroSection;
