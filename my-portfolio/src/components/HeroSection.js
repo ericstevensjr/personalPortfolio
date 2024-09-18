a74fe04b23537a08b2e86c5e0c1f4caef7d945dc
Eric Stevens Jr. <ericstevensjrprofessional@outlook.com>
2024-09-18 19:30:35 -0400

    Added photo and some other things

--- my-portfolio/src/components/HeroSection.js
+++ my-portfolio/src/components/HeroSection.js
@@ -1,11 +1,19 @@
 import usaFlag from "../assets/usaFlag.png";
 import flFlag from "../assets/flFlag.png";
+import mugshot from "../assets/beautiful.JPG"; // Add your photo here
 
 function HeroSection() {
   return (
     <section className="flex flex-col items-center justify-center text-center my-16">
+      {/* Add your photo here */}
+      <img
+        src={mugshot}
+        alt="A Photo of Eric Stevens Jr."
+        className="w-72 h-auto mb-6 border-4 border-gray-300 shadow-lg" // No rounding, subtle border and shadow
+      />
+
       <h1 className="text-5xl font-bold mb-4 text-gray-900 dark:text-gray-100 transition-colors duration-300">
-        Hi, I'm Eric Stevens
+        Hello, I'm Eric Stevens
       </h1>
       <p className="text-xl mb-6 text-gray-700 dark:text-gray-300 transition-colors duration-300">
         A United States Army Combat Infantryman turned Software Engineer and Businessman.
