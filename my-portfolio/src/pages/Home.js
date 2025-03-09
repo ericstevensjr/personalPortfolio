f60c008486ae23bc58c4d634fe9687369473514b
Eric Stevens Jr. <ericstevensjrprofessional@outlook.com>
2025-03-09 18:14:07 -0400

    Still debugging

--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -2,15 +2,17 @@ import HeroSection from '../components/HeroSection';
 import AboutMe from '../components/AboutMe';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
-import LatestBlogPost from '../components/LatestBlogPost';
+import LatestBlogPost from '../components/LatestBlogPost'; // Ensure this import exists
+
+console.log("Home Component Loaded"); // Debugging
 
 function Home() {
   return (
     <div className="container mx-auto p-4">
       <HeroSection />
       <AboutMe />
-      <LatestBlogPost />
       <Projects />
+      <LatestBlogPost />
       <Contact />
     </div>
   );
